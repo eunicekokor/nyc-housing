2cc9b98825ec6f4412955c12e5163e0c229098af
Sam Nnodim <sam.nnodim@gmail.com>
2016-04-11 12:47:05 -0400

    Adds neighborhood & code to redis store

--- get_data_from_quandl.py
+++ get_data_from_quandl.py
@@ -5,11 +5,19 @@
 #   + creates Flask app, and a REST API to poll
 
 # libraries and tings
+import Quandl as Q
 import config
-import requests
+import redis
 import json
+import time
 import csv
-import Quandl
+import sys
+
+# connect the redis store
+store = redis.Redis(db=0)
+
+# create array for list of hoods
+listOfHoods = []
 
 # get the api key from config
 apiKey = config.apiKey
@@ -17,8 +25,20 @@ apiKey = config.apiKey
 # parse the neighborhood codes csv
 with open('hood_codes.csv', 'rb') as csvfile:
     hoodreader = csv.reader(csvfile, delimiter=',')
+    # for each row in the csv, get the hood & code
     for row in hoodreader:
+        # only get NYC neighborhoods
         if row[1] == 'New York' and row[3] == 'New York':
+            # add the neighborhood to listOfHoods
             neighborhood = row[0]
-            hood_code = row[-1].split("|")[1]
-            print ( neighborhood + ': ' + hood_code)
+            hoodCode = row[-1].split("|")[1]
+            listOfHoods.append( neighborhood )
+
+            # ping the Quandl api
+            # https://www.quandl.com/data/ZILL/documentation/documentation
+            #quandlQuery = ('ZILL/N'+ hoodCode + '_A')
+            #data = Q.get(quandlQuery, authtoken=apiKey)
+            #data.head()
+
+            # add the pair to a redis store
+            store.set(neighborhood, hoodCode)
